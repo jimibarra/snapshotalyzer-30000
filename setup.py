46ffb9e0e1fd5493493b34e582982d9a9e8ef6f3
jimibarra <jimibarra18@hotmail.com>
2020-06-22 13:11:36 +0000

    Setup.py to enable building

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='snapshotalyzer-30000',
+    version='0.1',
+    author="Jim Ibarra",
+    description="Snapshotalyzer-30000 is a tool to manage snapshots",
+    license="GPLv3+",
+    packages=['shotty'],
+    url="https://github.com/jimibarra/snapshotalyzer-30000",
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        shotty=shotty.shotty:cli
+    ''',
+)
